6738abed0fef6a565c4e948f50ef1b7217bd10ff
TajnimaBintaNaz <tajnimavu10@gmail.com>
2022-02-01 16:13:14 +0600

    comparison added

---
+++ comparison.js
@@ -0,0 +1,15 @@
+
+//console.log(5 > 6);
+//console.log(5 < 6);
+
+//console.log(5 == 6);
+//console.log(6 == 6);
+
+//conlose.log(5 != 6);
+var mylove = 100;
+var hisLove = 99;
+
+console.log(mylove > hisLove);
+
+console.log(studyGood && goodPerson);
+console.log(isBanker || goodPerson);
---
+++ conditional.js
@@ -0,0 +1,14 @@
+var isFoodReady = true;
+
+if (isFoodReady == true) {
+
+    console.log('Momy i will it now');
+}
+
+var IphonePrice = 70000;
+var myBudget = 109500;
+
+if (IphonePrice < myBudget) {
+
+    console.log('iphone kine futani marbo!!');
+}
